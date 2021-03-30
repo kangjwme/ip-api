17de103894261a8d4050ebedd2b12354b16decc2
kangjw <71870130+kangjwme@users.noreply.github.com>
2021-03-30 10:10:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,14 @@ import requests
 import json
 try:
     while True:
-      key = input("請輸入ipinfo.io中所提供的金鑰")
-
       print("=========本機資訊=========")
-
-      local = requests.get("https://ipinfo.io/?token=%s" % (key))
+      local = requests.get("http://ip-api.com/json/")
       info = json.loads(local.text)
-
-      print("主機位置：\t"+info["ip"])
-      print("主機名稱：\t"+info["hostname"])
+      print("主機IP：\t"+info["query"])
       print("所在城市：\t"+info["city"])
-      print("所在國家：\t"+info["region"]+","+info["country"])
-      print("電信組織\t"+info["org"])
+      print("所在國家：\t"+info["country"]+","+info["regionName"]+","+info["city"])
       print("當前時區\t"+info["timezone"])
+      print("電信提供商ISP\t"+info["isp"])
+      print("AS編號\t"+info["as"])
 except EOFError:
     pass
